66ebe2eb6392a0bb3c0c02d692f4f18549197508
AMC112002 <alejandromateocapilla@gmail.com>
2022-11-03 13:01:06 +0100

    MUSHO BETI

--- src/main/java/org/springframework/samples/petclinic/product/Product.java
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -1,13 +1,28 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product {
-    Integer id;
-    String name;
-    double price;
-    ProductType productType;
+@Entity
+@Table(name = "products")
+public class Product extends BaseEntity {
+    @NotEmpty
+    @Size(min = 3, max = 50)
+    private String name;
+    @NotEmpty
+    @PositiveOrZero
+    private double price;
+//    @ManyToOne
+//    private ProductType productType;
 }
